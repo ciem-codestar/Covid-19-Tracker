0c97341d578ea03d9d43db3f46ad4101858fe98b
ciem-codestar <110713665+ciem-codestar@users.noreply.github.com>
2022-08-08 11:43:03 +0530

    All files are uploaded

---
+++ LiveStatus/map.js
@@ -0,0 +1,16 @@
+// Map show hide
+function hello() {
+    var location=get
+       var max = document.getElementById("plus");
+    var min = document.getElementById("minus");
+    if (location.style.display == none) {
+        location.style.display = 'block';
+        max.style.display = 'none';
+        min.style.display = 'block';
+    }
+    else {
+        location.style.display = 'none';
+        max.style.display = 'block';
+        min.style.display = 'none';
+    }
+}
